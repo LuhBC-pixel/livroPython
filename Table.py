80a5facb61fbe3a82ae4d9a13a1f2191c20581fe
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-08-07 20:20:16 -0300

    Update Table.py

--- Table.py
+++ Table.py
@@ -1,8 +1,13 @@
 def printTables(listaString):
     colWidth = [0] * len(listaString)
 
-    for i in range(len(colWidth)):
-        colWidth[i] = max(listaString[i])
+    for i in range(len(listaString)):
+        maior = 0
+        for j in range(len(listaString[i])):
+            if len(listaString[i][j]) > maior:
+                maior = len(listaString[i][j])
+                letra = listaString[i][j]
+        colWidth[i] = letra
 
     stringLonga = max(colWidth)
     tamanho = len(stringLonga)
@@ -12,7 +17,7 @@ def printTables(listaString):
             if j >= len(listaString) - 1:
                 print(str(listaString[j][x]).rjust(tamanho))
             else:
-                print(str(listaString[j][x]).rjust(tamanho), end='\t')
+                print(str(listaString[j][x]).rjust(tamanho), end=' ')
 
 tableData = [['apples', 'oranges', 'cherries', 'banana'], ['Alice', 'Bob', 'Carol', 'David'], ['dogs', 'cats', 'moose', 'goose']]
 printTables(tableData)
